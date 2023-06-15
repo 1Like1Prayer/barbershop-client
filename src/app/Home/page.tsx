c7583720e24d3667ab78190fd51b807273522c41
Tamir <tamirdude96@gmail.com>
2023-06-15 20:35:00 +0300

    first commit

---
+++ src/app/Home/page.tsx
@@ -0,0 +1,6 @@
+'use client';
+// import Example from '../../../components/ui/tailwind-drawer';
+
+import Navbar from '@/app/components/navbar';
+const temp = () => <div>hello world</div>;
+export default Navbar;
